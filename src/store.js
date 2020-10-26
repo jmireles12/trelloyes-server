7c8be2d12c49bd1e9f90da53917319c9fcee8cee
Juan Mireles <jmireles12@gmail.com>
2020-10-26 14:44:24 -0500

    added router

---
+++ src/store.js
@@ -0,0 +1,13 @@
+const cards = [{
+    id: 1,
+    title: 'task One',
+    content: 'This is card one'
+}]
+
+const lists = [{
+    id: 1,
+    header: 'List One',
+    cardIds: [1]
+}]
+
+module.exports = { cards, lists }
